38183e0a6de13f8dc4d4220abb873e2e8bdffcfb
samaahmed19 <sama23012612@gmail.com>
2025-08-20 03:17:45 +0300

    Fake App

--- app/src/main/java/com/example/movie_discovery/MainActivity.kt
+++ app/src/main/java/com/example/movie_discovery/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.movie_discovery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,5 +12,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.movie_discovery.ui.theme.Movie_DiscoveryTheme
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Movie_DiscoveryTheme {
+                Scaffold { innerPadding ->
+                    Greeting(
+                        name = "Movie Discovery",
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    )
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Movie_DiscoveryTheme {
+        Greeting("Android")
+    }
+}
