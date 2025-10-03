378286ef1f06027bfb32ce3791ee661e74d7607b
samaahmed19 <sama23012612@gmail.com>
2025-10-03 12:29:45 +0300

    Implement SplashScreen navigation

--- app/src/main/java/com/example/movie_discovery/MainActivity.kt
+++ app/src/main/java/com/example/movie_discovery/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.example.movie_discovery
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.movie_discovery.ui.theme.Movie_DiscoveryTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Movie_DiscoveryTheme {
-                Scaffold { innerPadding ->
-                    Greeting(
-                        name = "Movie Discovery",
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize()
-                    )
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Movie_DiscoveryTheme {
-        Greeting("Android")
+
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
+
+        composable("splash") {
+            SplashScreen {
+                navController.navigate("home") {
+
+                    popUpTo("splash") { inclusive = true }
+                }
+            }
+        }
+
+
+        composable("home") {
+            HomeScreen()
+        }
     }
 }
