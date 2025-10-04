b27cad4b769ea26084137b9140aa573ece390946
mouhy <mouhab46@gmail.com>
2025-10-04 12:52:20 +0300

    add coil compose library

--- app/src/main/java/com/example/movie_discovery/Search_Screen.kt
+++ app/src/main/java/com/example/movie_discovery/Search_Screen.kt
@@ -1,4 +1,21 @@
 package com.example.movie_discovery
 
-class Search_Screen {
-}
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.movie_discovery.ui.theme.*
