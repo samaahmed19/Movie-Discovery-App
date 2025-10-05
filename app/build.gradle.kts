b73b2e2adf613d1d63daa302e6193326f21fcf7c
mouhy <mouhab46@gmail.com>
2025-10-05 19:58:34 +0300

    add NeonText

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 }
 
 dependencies {
+    implementation("br.com.devsrsouza.compose.icons:simple-icons:1.1.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
