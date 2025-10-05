d2a92e56fcb61d89c78acc5736c85bad9c3c82e6
eng-boda <abdelrahman6as@gmail.com>
2025-10-05 11:08:39 +0300

    Category Card Box Body

--- app/src/main/java/com/example/movie_discovery/Category_Card.kt
+++ app/src/main/java/com/example/movie_discovery/Category_Card.kt
@@ -1,13 +1,25 @@
 package com.example.movie_discovery
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.jetbrains.annotations.Async
+import coil.compose.AsyncImage
+import com.example.movie_discovery.Category
+import com.example.movie_discovery.ui.theme.*
 
 data class Category(
     val name: String,
@@ -20,5 +32,28 @@ fun CategoryCard(category: Category){
         modifier = Modifier
             .aspectRatio(1f)
             .clip(RoundedCornerShape(8.dp))
-    )
+    ){
+        AsyncImage(
+            model = category.imageUrl,
+            contentDescription = category.name,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(CardBackground)
+        )
+
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(CardBackground.copy(alpha = 0.5f))
+        )
+
+        Text(
+            text = category.name,
+            color = TextPrimary,
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
 }
