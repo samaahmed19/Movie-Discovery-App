96250009f7c0d66945d1945b3690d604243f14dc
mouhy <mouhab46@gmail.com>
2025-10-06 15:05:18 +0300

    Add NeonText

--- app/src/main/java/com/example/movie_discovery/SignUpScreen.kt
+++ app/src/main/java/com/example/movie_discovery/SignUpScreen.kt
@@ -1,4 +1,66 @@
 package com.example.movie_discovery
 
-class SignUpScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import compose.icons.SimpleIcons
+import compose.icons.simpleicons.Google
+import com.example.movie_discovery.ui.theme.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.graphics.Color
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen(navController: NavController?) {
+    val scrollState = rememberScrollState()
+
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    val backgroundBrush = Brush.verticalGradient(
+        colors = listOf(DarkNavy, CardBackground.copy(alpha = 0.8f), DarkNavy)
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundBrush)
+            .padding(24.dp)
+            .verticalScroll(scrollState)
+            .imePadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(bottom = 40.dp)
+        ) {
+            com.example.movie_discovery.NeonText(
+                text = "Movie",
+                neonColor = Color.Cyan.copy(alpha = 0.9f)
+            )
+            com.example.movie_discovery.NeonText(
+                text = "Discovery",
+                neonColor = AccentRed.copy(alpha = 0.8f)
+            )
+    }
+    }
 }
