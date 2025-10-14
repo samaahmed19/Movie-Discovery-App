d1427f39fb0d8870c0a007f69719a8449a6648cd
MirnaElsheikh <mernamahmoud300@gmail.com>
2025-10-15 01:17:46 +0300

    remove settings icon , add dark mode switch

--- app/src/main/java/com/example/movie_discovery/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movie_discovery/ui/theme/Theme.kt
@@ -1,13 +1,13 @@
 package com.example.movie_discovery.ui.theme
 
 import android.os.Build
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -17,6 +17,7 @@ fun MoviesTheme(
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
+
     val DarkColorScheme = darkColorScheme(
         primary = AccentRed,
         secondary = Gold,
@@ -48,8 +49,16 @@ fun MoviesTheme(
         else -> LightColorScheme
     }
 
+
+    val animatedColorScheme = colorScheme.copy(
+        background = animateColorAsState(colorScheme.background).value,
+        surface = animateColorAsState(colorScheme.surface).value
+    )
+
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = animatedColorScheme,
+        typography = Typography,
         content = content
     )
+
 }
