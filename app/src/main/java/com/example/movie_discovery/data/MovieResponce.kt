ec518022abace910ca72e6477df3e349b5bb312c
MirnaElsheikh <mernamahmoud300@gmail.com>
2025-10-16 15:02:00 +0300

    move data classes to data package and fix API response types

--- app/src/main/java/com/example/movie_discovery/data/MovieResponce.kt
+++ app/src/main/java/com/example/movie_discovery/data/MovieResponce.kt
@@ -2,34 +2,12 @@ package com.example.movie_discovery.data
 
 import com.google.gson.annotations.SerializedName
 
-data class MovieResponse(val results: List<MovieDetailsResponse>)
+data class MovieResponse(
+    val results: List<MovieDetailsResponse>
+)
 
 data class MovieSearchResponse(
     val results: List<Movie> = emptyList(),
     val page: Int = 1,
-    @SerializedName("total_pages")
-    val totalPages: Int = 0
-)
-
-data class CategoryMoviesResponse(
-    val results: List<Movie> = emptyList(),
-    val page: Int = 1,
-    @SerializedName("total_pages")
-    val totalPages: Int = 0
-)
-
-data class Movie(
-    val id: Int = 0,
-    val title: String = "",
-    val overview: String,
-    @SerializedName("poster_path")
-    val poster_path: String? = null,
-    @SerializedName("backdrop_path")
-    val backdropPath: String? = null,
-    @SerializedName("release_date")
-    val release_date: String = "",
-    @SerializedName("vote_average")
-    val voteAverage: Double = 0.0,
-    @SerializedName("genre_ids")
-    val genreIds: List<Int> = emptyList()
+    @SerializedName("total_pages") val totalPages: Int = 0
 )
