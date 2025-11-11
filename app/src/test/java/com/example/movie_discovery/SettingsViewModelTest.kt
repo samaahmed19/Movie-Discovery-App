5cde5a11c5808c4325dba24f5227a4060eb708d1
Lina Ashraf <ashraflina21@gmail.com>
2025-11-11 11:44:56 +0200

    Settings test

---
+++ app/src/test/java/com/example/movie_discovery/SettingsViewModelTest.kt
@@ -0,0 +1,29 @@
+package com.example.movie_discovery
+import android.content.Context
+import com.example.movie_discovery.Viewmodels.UserSettings
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SettingsViewModelTest {
+
+    private lateinit var viewModel: FakeSettingsViewModel
+    private lateinit var mockContext: Context
+
+    @Before
+    fun setUp() {
+        viewModel = FakeSettingsViewModel()
+        mockContext = Mockito.mock(Context::class.java)
+    }
+
+    @Test
+    fun changeLanguage_shouldUpdateLanguage() = runTest {
+        viewModel.changeLanguage("ar", mockContext)
+        val settings = viewModel.userSettings.first()
+        assertEquals("ar", settings.language)
+    }
