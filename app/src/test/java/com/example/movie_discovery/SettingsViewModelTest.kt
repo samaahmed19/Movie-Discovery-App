8d0f65afa99d5fa3131080d0472d572e6f1f102a
Lina Ashraf <ashraflina21@gmail.com>
2025-11-11 11:50:17 +0200

    completting Settings test

--- app/src/test/java/com/example/movie_discovery/SettingsViewModelTest.kt
+++ app/src/test/java/com/example/movie_discovery/SettingsViewModelTest.kt
@@ -35,3 +35,45 @@ class SettingsViewModelTest {
         assertEquals("Cairo", settings.fontType)
     }
 
+    @Test
+    fun changeFontSize_shouldUpdateFontSize() = runTest {
+        viewModel.changeFontSize(22f)
+        val settings = viewModel.userSettings.first()
+        assertEquals(22f, settings.fontSize)
+    }
+
+    @Test
+    fun resetToDefault_shouldReturnDefaultValues() = runTest {
+        viewModel.changeLanguage("ar", mockContext)
+        viewModel.changeFontType("Roboto")
+        viewModel.changeFontSize(20f)
+        viewModel.resetToDefault(mockContext)
+        val settings = viewModel.userSettings.first()
+        assertEquals("en", settings.language)
+        assertEquals("Momo", settings.fontType)
+        assertEquals(18f, settings.fontSize)
+    }
+}
+
+class FakeSettingsViewModel {
+
+    private val _userSettings = kotlinx.coroutines.flow.MutableStateFlow(UserSettings())
+    val userSettings: kotlinx.coroutines.flow.StateFlow<UserSettings> = _userSettings
+
+    fun changeLanguage(lang: String, context: Context) {
+        _userSettings.value = _userSettings.value.copy(language = lang)
+    }
+
+    fun changeFontType(font: String) {
+        _userSettings.value = _userSettings.value.copy(fontType = font)
+    }
+
+    fun changeFontSize(size: Float) {
+        _userSettings.value = _userSettings.value.copy(fontSize = size)
+    }
+
+    fun resetToDefault(context: Context) {
+        _userSettings.value = UserSettings()
+    }
+}
+
